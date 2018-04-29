04f027dfb734967c383d0c9057c1a7994c6b3212
Shikyo Kira <alexandros_ff9@hotmail.com>
2018-04-29 20:34:02 +0800

    issue #2, issue #20

--- add animation/gettemplate.cpp
+++ add animation/gettemplate.cpp
@@ -4,136 +4,185 @@ using namespace std;
 
 getTemplate::getTemplate()
 {
-	string templateDirectory = "behavior templates/";
+	string templateDirectory = "behavior templates\\";
 	string newpath;
-
-	vecstr folderlist;
 	vecstr codelist;
-	vecstr filelist;
+	read_directory(templateDirectory, codelist);
 
-	read_directory(templateDirectory, folderlist);
-
-	for (unsigned int l = 0; l < folderlist.size(); l++)
+	for (unsigned int k = 0; k < codelist.size(); ++k)
 	{
-		if (folderlist[l].find(".", 0) == string::npos)
+		if (codelist[k].find(".", 0) == string::npos)
 		{
-			newpath = templateDirectory + folderlist[l];
+			newpath = templateDirectory + codelist[k];
 			boost::filesystem::path FOF(newpath);
+
+			if (boost::iequals(codelist[k], "t") || boost::iequals(codelist[k], "aaprefix") || boost::iequals(codelist[k], "aaset") || boost::iequals(codelist[k], "md") || boost::iequals(codelist[k], "rd") || boost::iequals(codelist[k], "+"))
+			{
+				cout << "ERROR(3009): Unable to use " << codelist[k] << " as animation code. " << codelist[k] << " has been registered into the engine by default. Please contact the template creator" << endl << "Animation Code: " << codelist[k] << endl << endl;
+				error = true;
+				return;
+			}
 
 			if (boost::filesystem::is_directory(FOF))
 			{
-				read_directory(newpath, codelist);
+				vecstr folderlist;
+				read_directory(newpath, folderlist);
+				bool isOptionExist = false;
+				bool registered = false;
+				boost::filesystem::path animData;
+				unordered_map<string, unordered_map<int, bool>> isStateJoint;		// behavior, node(function) ID, true/false; is this node(function) joining the animation template with the main branch?
+				vector<boost::filesystem::path> pathVector;
 
-				for (unsigned int k = 0; k < codelist.size(); k++)
+				for (unsigned int l = 0; l < folderlist.size(); ++l)
 				{
-					newpath = templateDirectory + folderlist[l] + "/" + codelist[k];
+					newpath = templateDirectory + codelist[k] + "\\" + folderlist[l];
 					boost::filesystem::path FOF2(newpath);
 
-					if (boost::filesystem::is_directory(FOF2))
+					if (boost::iequals(folderlist[l], "option_list.txt") && !boost::filesystem::is_directory(FOF2))
 					{
+						OptionList option(newpath, codelist[k]);
+
+						if (error)
+						{
+							return;
+						}
+
+						option.setDebug(debug);
+						optionlist[codelist[k]] = option;
+						templatelist[codelist[k]] = true;
+						isOptionExist = true;
+					}
+					else if (boost::iequals(folderlist[l], "animationdatasinglefile") && boost::filesystem::is_directory(FOF2))
+					{
+						animData = FOF2;
+					}
+					else
+					{
+						pathVector.push_back(FOF2);
+					}
+				}
+
+				for (unsigned int l = 0; l < pathVector.size(); ++l)
+				{
+					if (boost::filesystem::is_directory(pathVector[l]))
+					{
+						vecstr filelist;
+						newpath = pathVector[l].string();
 						read_directory(newpath, filelist);
-						bool isOptionExist = false;
+						string behaviorFolder = pathVector[l].stem().string();
+						string lowerBehaviorFolder = boost::algorithm::to_lower_copy(behaviorFolder);
 						bool noGroup = true;
 
 						for (unsigned int i = 0; i < filelist.size(); i++)
 						{
-							newpath = templateDirectory + folderlist[l] + "/" + codelist[k] + "/" + filelist[i];
+							newpath = templateDirectory + codelist[k] + "\\" + behaviorFolder + "\\" + filelist[i];
 							boost::filesystem::path file(newpath);
 							boost::algorithm::to_lower(filelist[i]);
 							boost::algorithm::to_lower(codelist[k]);
 
-							if (filelist[i] == "option_list.txt" && !boost::filesystem::is_directory(file))
+							if (!boost::filesystem::is_directory(file))
 							{
-								OptionList option(newpath, codelist[k]);
-
-								if (error)
+								if ((filelist[i] == codelist[k] + "_group.txt" || filelist[i] == codelist[k] + "_group.nmd"))
 								{
-									return;
-								}
-
-								option.setDebug(debug);
-								optionlist[codelist[k]] = option;
-								grouplist[folderlist[l]].push_back(codelist[k]);
-								templatelist[codelist[k]] = true;
-								isOptionExist = true;
-							}
-							else if ((filelist[i] == codelist[k] + "_group.txt" || filelist[i] == codelist[k] + "_group.nmd") && !boost::filesystem::is_directory(file))
-							{
-								if (behaviortemplate[codelist[k] + "_group"].size() == 0)
-								{
-									behaviortemplate[codelist[k] + "_group"] = GetFunctionLines(newpath);
-									noGroup = false;
-								}
-								else
-								{
-									cout << "ERROR(1019): Duplicated template file detected. Please contact the template creator" << endl << "File: " << newpath << endl << endl;
-									error = true;
-									return;
-								}
-							}
-							else if ((filelist[i] == codelist[k] + "_master.txt" || filelist[i] == codelist[k] + "_master.nmd") && !boost::filesystem::is_directory(file))
-							{
-								if (behaviortemplate[codelist[k] + "_master"].size() == 0)
-								{
-									behaviortemplate[codelist[k] + "_master"] = GetFunctionLines(newpath);
-								}
-								else
-								{
-									cout << "ERROR(1019): Duplicated template file detected. Please contact the template creator" << endl << "File: " << newpath << endl << endl;
-									error = true;
-									return;
-								}
-							}
-							else if ((filelist[i] == codelist[k] + ".txt" || filelist[i] == codelist[k] + ".nmd") && !boost::filesystem::is_directory(file))
-							{
-								if (behaviortemplate[codelist[k]].size() == 0)
-								{
-									behaviortemplate[codelist[k]] = GetFunctionLines(newpath);
-								}
-								else
-								{
-									cout << "ERROR(1019): Failed to identify template file. Please contact the template creator" << endl << "File: " << newpath << endl << endl;
-									error = true;
-									return;
-								}
-							}
-							else if (filelist[i][0] == '#' && !boost::filesystem::is_directory(file))
-							{
-								string number = boost::regex_replace(string(filelist[i]), boost::regex("[^0-9]*([0-9]+).*"), string("\\1"));
-
-								if (filelist[i] == "#" + number + ".txt")
-								{
-									existingFunctionID[codelist[k]].push_back(stoi(number));
-								}
-
-								vecstr storeline = GetFunctionLines(newpath);
-								bool isJoint = false;
-
-								for (unsigned int j = 0; j < storeline.size(); j++)
-								{
-									if (storeline[j].find("<!-- NEW ^" + codelist[k]) != string::npos)
+									if (behaviortemplate[codelist[k] + "_group"][lowerBehaviorFolder].size() == 0)
 									{
-										if (!isJoint)
+										behaviortemplate[codelist[k] + "_group"][lowerBehaviorFolder] = GetFunctionLines(newpath);
+										noGroup = false;
+
+										if (error)
 										{
-											isJoint = true;
-										}
-										else
-										{
-											mainBehaviorJoint[codelist[k]] = stoi(number);
-											break;
+											return;
 										}
 									}
-
-									if (storeline[j].find("#" + codelist[k]) != string::npos)
+									else
 									{
-										if (!isJoint)
+										cout << "ERROR(1019): Duplicated template file detected. Please contact the template creator" << endl << "File: " << newpath << endl << endl;
+										error = true;
+										return;
+									}
+								}
+								else if ((filelist[i] == codelist[k] + "_master.txt" || filelist[i] == codelist[k] + "_master.nmd"))
+								{
+									if (behaviortemplate[codelist[k] + "_master"][lowerBehaviorFolder].size() == 0)
+									{
+										behaviortemplate[codelist[k] + "_master"][lowerBehaviorFolder] = GetFunctionLines(newpath);
+										noGroup = false;
+
+										if (error)
 										{
-											isJoint = true;
+											return;
 										}
-										else
+									}
+									else
+									{
+										cout << "ERROR(1019): Duplicated template file detected. Please contact the template creator" << endl << "File: " << newpath << endl << endl;
+										error = true;
+										return;
+									}
+								}
+								else if (filelist[i] == codelist[k] + ".txt")
+								{
+									registered = true;
+									grouplist[lowerBehaviorFolder].push_back(codelist[k]);
+
+									if (behaviortemplate[codelist[k]][lowerBehaviorFolder].size() == 0)
+									{
+										behaviortemplate[codelist[k]][lowerBehaviorFolder] = GetFunctionLines(newpath);
+
+										if (error)
 										{
-											mainBehaviorJoint[codelist[k]] = stoi(number);
-											break;
+											return;
+										}
+									}
+									else
+									{
+										cout << "ERROR(1019): Failed to identify template file. Please contact the template creator" << endl << "File: " << newpath << endl << endl;
+										error = true;
+										return;
+									}
+								}
+								else if (filelist[i][0] == '#')
+								{
+									string number = boost::regex_replace(string(filelist[i]), boost::regex("[^0-9]*([0-9]+).*"), string("\\1"));
+
+									if (filelist[i] == "#" + number + ".txt")
+									{
+										existingFunctionID[codelist[k]][lowerBehaviorFolder].push_back(stoi(number));
+									}
+
+									vecstr storeline = GetFunctionLines(newpath);
+									bool isJoint = false;
+									bool isStateMachine = false;
+
+									for (unsigned int j = 0; j < storeline.size(); j++)
+									{
+										if (storeline[j].find("class=\"hkbStateMachine\" signature=\"") != string::npos)
+										{
+											isStateMachine = true;
+										}
+
+										if (isStateMachine && storeline[j].find("<!-- NEW ^" + codelist[k]) != string::npos)
+										{
+											if (!isJoint)
+											{
+												isJoint = true;
+											}
+										}
+										else if (isStateMachine && storeline[j].find("<!-- CLOSE -->") != string::npos)
+										{
+											if (isJoint)
+											{
+												isJoint = false;
+											}
+										}
+
+										if (isStateMachine && storeline[j].find("#" + codelist[k]) != string::npos)
+										{
+											if (isJoint)
+											{
+												isStateJoint[lowerBehaviorFolder][stoi(number)] = true;
+												break;
+											}
 										}
 									}
 								}
@@ -145,36 +194,86 @@ getTemplate::getTemplate()
 							}
 						}
 
+						if (optionlist[codelist[k]].multiState[lowerBehaviorFolder].size() > 1)
+						{
+							if (isStateJoint[lowerBehaviorFolder].size() == 0)
+							{
+								cout << "ERROR(1074): Joint not found. Animation will not work. Please contact the template creator" << endl << "File: " << templateDirectory << codelist[k] << "\\option_list.txt" << endl << endl;
+								error = true;
+								return;
+							}
+							else if (isStateJoint[lowerBehaviorFolder].size() != optionlist[codelist[k]].multiState[lowerBehaviorFolder].size())
+							{
+								cout << "ERROR(1073): Potential wrong or missing state reference. All joints must be registered in option_list.txt when using multi joints. Please contact the template creator" << endl << "File: " << templateDirectory << codelist[k] << "\\option_list.txt" << endl << endl;
+								error = true;
+								return;
+							}
+
+							for (auto it = optionlist[codelist[k]].multiState[lowerBehaviorFolder].begin(); it != optionlist[codelist[k]].multiState[lowerBehaviorFolder].end(); ++it)
+							{
+								if (isStateJoint[lowerBehaviorFolder][it->second])
+								{
+									mainBehaviorJoint[codelist[k]][lowerBehaviorFolder][it->first] = it->second;
+								}
+								else
+								{
+									cout << "ERROR(1075): Missing state reference detected. All joints must be registered in option_list.txt when using multi joints. Please contact the template creator" << endl << "File: " << templateDirectory << codelist[k] << "\\option_list.txt" << endl << endl;
+									error = true;
+									return;
+								}
+							}
+						}
+						else
+						{
+							if (optionlist[codelist[k]].multiState[lowerBehaviorFolder].size() == 1)
+							{
+								cout << "WARNING: Only 1 State Machine detected. Multi StateMachine ignored. Please contact the template creator" << endl << "File: " << templateDirectory << codelist[k] << "\\option_list.txt" << endl << endl;
+							}
+
+							if (isStateJoint[lowerBehaviorFolder].size() > 1)
+							{
+								cout << "ERROR(1072): Potential wrong state reference. Template containing multiple StateMachine joints requires registration in option_list.txt to work. Please contact the template creator" << endl << "File: " << templateDirectory << codelist[k] << "\\option_list.txt" << endl << endl;
+								error = true;
+								return;
+							}
+							else if (isStateJoint[lowerBehaviorFolder].size() == 0)
+							{
+								cout << "ERROR(1074): Joint not found. Animation will not work. Please contact the template creator" << endl << "File: " << templateDirectory << codelist[k] << "\\option_list.txt" << endl << endl;
+								error = true;
+								return;
+							}
+
+							mainBehaviorJoint[codelist[k]][lowerBehaviorFolder][0] = isStateJoint[lowerBehaviorFolder].begin()->first;
+						}
+
 						// Error checking
 						if (noGroup)
 						{
-							if (optionlist[codelist[k]].groupMin != -1)
+							if (!optionlist[codelist[k]].ignoreGroup)
 							{
-								cout << "ERROR(1061): " << codelist[k] << "_group.txt file cannot be found. Unable to proceed when \"minimum\" is used without " << codelist[k] << "_group.txt. Please contact the template creator" << endl << "File Path: " << templateDirectory << folderlist[l] << "/" << codelist[k] << endl << endl;
+								if (optionlist[codelist[k]].groupMin != -1 || optionlist[codelist[k]].ruleOne.size() != 0 || optionlist[codelist[k]].ruleTwo.size() != 0)
+								{
+									cout << "ERROR(1061): " << codelist[k] << "_group.txt file cannot be found. Please contact the template creator" << endl << "File Path: " << templateDirectory << behaviorFolder << "\\" << codelist[k] << endl << endl;
+									error = true;
+									return;
+								}
+
+								if (behaviortemplate[codelist[k] + "_master"].size() != 0)
+								{
+									cout << "ERROR(1033): Missing group animation template. Master animation template is only available to group animation template. Please contact the template creator" << endl << "File Path: " << templateDirectory << behaviorFolder << "\\" << codelist[k] << endl << endl;
+									error = true;
+									return;
+								}
+							}
+						}
+						else
+						{
+							if (optionlist[codelist[k]].ignoreGroup)
+							{
+								cout << "ERROR(1079): " << codelist[k] << "_group.txt detected. Conflict with ignore_group condition in option_list.txt. Please contact the template creator" << endl << "File Path: " << templateDirectory << codelist[k] << "\\option_list.txt" << endl << endl;
 								error = true;
 								return;
 							}
-
-							if (behaviortemplate[codelist[k] + "_master"].size() != 0)
-							{
-								cout << "ERROR(1033): Missing group animation template. Master animation template is only available to group animation template. Please contact the template creator" << endl << "File Path: " << templateDirectory << folderlist[l] << "/" << codelist[k] << endl << endl;
-								error = true;
-								return;
-							}
-						}
-
-						if (!isOptionExist)
-						{
-							cout << "ERROR(1021): Missing option list file. Please contact the template creator" << endl << "File: " << newpath << endl << endl;
-							error = true;
-							return;
-						}
-
-						if ((optionlist[codelist[k]].groupMin != 0 || optionlist[codelist[k]].ruleOne.size() != 0 || optionlist[codelist[k]].ruleTwo.size() != 0) && noGroup)
-						{
-							cout << "ERROR(1024): Missing group file. Please contact the template creator" << endl << "File: " << newpath << endl << endl;
-							error = true;
-							return;
 						}
 
 						filelist.clear();
@@ -186,7 +285,113 @@ getTemplate::getTemplate()
 					return;
 				}
 
-				codelist.clear();
+				if (animData.string().length() != 0)
+				{
+					string animDataPath = animData.string();
+					vecstr characlist;
+					read_directory(animDataPath, characlist);
+
+					for (unsigned int l = 0; l < characlist.size(); ++l)
+					{
+						newpath = animDataPath + "\\" + characlist[l];
+						boost::filesystem::path FOF3(newpath);
+
+						if (boost::filesystem::is_directory(FOF3))
+						{
+							vecstr headerlist;
+							read_directory(newpath, headerlist);
+
+							for (unsigned int i = 0; i < headerlist.size(); ++i)
+							{
+								newpath = newpath + "\\" + headerlist[i];
+								boost::filesystem::path FOF4(newpath);
+
+								if (!boost::filesystem::is_directory(FOF4))
+								{
+									string lowerBehaviorFolder = boost::algorithm::to_lower_copy(animData.stem().string()) + " " + characlist[l];
+
+									if (headerlist[i] == codelist[k] + ".txt")
+									{
+										registered = true;
+										grouplist[lowerBehaviorFolder].push_back(codelist[k]);
+
+										if (behaviortemplate[codelist[k]][lowerBehaviorFolder].size() == 0)
+										{
+											behaviortemplate[codelist[k]][lowerBehaviorFolder] = GetFunctionLines(newpath);
+
+											if (error)
+											{
+												return;
+											}
+										}
+										else
+										{
+											cout << "ERROR(1019): Failed to identify template file. Please contact the template creator" << endl << "File: " << newpath << endl << endl;
+											error = true;
+											return;
+										}
+									}
+									else
+									{
+										stringstream sstream(FOF4.stem().string());
+										istream_iterator<string> ssbegin(sstream);
+										istream_iterator<string> ssend;
+										vecstr fileparts(ssbegin, ssend);
+										copy(fileparts.begin(), fileparts.end(), fileparts.begin());
+
+										if (fileparts.size() < 3 && fileparts.size() != 0)
+										{
+											if (fileparts.size() == 1 && !isOnlyNumber(fileparts[0]))
+											{
+												cout << "ERROR(2004): Invalid file name. File name must only contain  either <modcode>$<id>, <id>, \"$header$\", \"$info header$\" or <animation clip name> <id>. Please contact the mod author" << endl << "File: " << newpath << endl << endl;
+												error = true;
+												return;
+											}
+											else if (fileparts.size() == 2 && (!hasAlpha(fileparts[0]) || !isOnlyNumber(fileparts[1])))
+											{
+												cout << "ERROR(2004): Invalid file name. File name must only contain  either <modcode>$<id>, <id>, \"$header$\", \"$info header$\" or <animation clip name> <id>. Please contact the mod author" << endl << "File: " << newpath << endl << endl;
+												error = true;
+												return;
+											}
+
+											if (characterHeaders.find(characlist[l]) == characterHeaders.end())
+											{
+												cout << "ERROR(3011): Unregistered character detected. Please re-run Update Patcher to fix it" << endl << "Character: " << animDataPath << "\\" << characlist[l] << endl << endl;
+												error = true;
+												return;
+											}
+
+											if (behaviortemplate[codelist[k]][lowerBehaviorFolder].size() == 0)
+											{
+												behaviortemplate[codelist[k]][lowerBehaviorFolder] = GetFunctionLines(newpath);
+
+												if (error)
+												{
+													return;
+												}
+											}
+											else
+											{
+												cout << "ERROR(1019): Duplicated template file detected. Please contact the template creator" << endl << "File: " << newpath << endl << endl;
+												error = true;
+												return;
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+
+				if (!isOptionExist && registered)
+				{
+					cout << "ERROR(1021): Missing option list file. Please contact the template creator" << endl << "File: " << newpath << endl << endl;
+					error = true;
+					return;
+				}
+				
+				folderlist.clear();
 			}
 		}
 
--- add animation/gettemplate.h
+++ add animation/gettemplate.h
@@ -5,13 +5,12 @@
 
 struct getTemplate
 {
-	std::unordered_map<std::string, OptionList> optionlist;
-	std::unordered_map<std::string, vecstr> grouplist;
-	std::unordered_map<std::string, bool> templatelist;
-	std::unordered_map<std::string, vecstr> behaviortemplate;
-	std::unordered_map<std::string, std::vector<int>> existingFunctionID;
-	std::unordered_map<std::string, vecstr> templatefilelist;
-	std::unordered_map<std::string, int> mainBehaviorJoint;
+	std::unordered_map<std::string, OptionList> optionlist;													// animation code, option list; access point to option list from animation code
+	std::unordered_map<std::string, vecstr> grouplist;														// behavior file, list of animation code(in vector of string); assign animation to their respective behavior
+	std::unordered_map<std::string, bool> templatelist;														// animation code, true/false; register template
+	std::unordered_map<std::string, std::unordered_map<std::string, vecstr>> behaviortemplate;				// animation code, behavior, template file(in vector of string); store template in memory
+	std::unordered_map<std::string, std::unordered_map<std::string, std::vector<int>>> existingFunctionID;	// animation code, behavior, list of node ID(in vector of int); node(function) from the master branch that is/are used in the generation of animation
+	std::unordered_map<std::string, std::unordered_map<std::string, std::map<int, int>>> mainBehaviorJoint;	// animation code, behavior, state number, node(function) ID; record the node that is joint with the master branch
 
 	getTemplate();
 };
