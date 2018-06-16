bf88abd40f64630397252da98878d2115f7627a5
Shikyo Kira <alexandros_ff9@hotmail.com>
2018-06-16 16:13:42 +0800

    issue #47

--- Nemesis Main GUI/main.cpp
+++ Nemesis Main GUI/main.cpp
@@ -1,9 +1,11 @@
 #include "NemesisMainGUI.h"
+#include "MultiInstanceCheck.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	programInitiateCheck();
 	NemesisMainGUI w;
 
 #ifndef DEBUG
