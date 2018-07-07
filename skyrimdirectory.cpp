46fddab283c873d5e3a1fbe4c02a96f6ea8d5352
Shikyo Kira <alexandros_ff9@hotmail.com>
2018-07-07 21:58:33 +0800

    minor tweak

--- skyrimdirectory.cpp
+++ skyrimdirectory.cpp
@@ -5,8 +5,16 @@ using namespace std;
 DataPath::DataPath()
 {
 	namespace bf = boost::filesystem;
-	bf::path path(bf::current_path());
-	// bf::path path("E:\\C++\\Project 2\\Nemesis Main GUI\\data\\meshes");
+	bf::path path;
+
+	if (bf::current_path().string() == "E:\\C++\\Project 2\\Nemesis Main GUI")
+	{
+		path = bf::path("E:\\C++\\Project 2\\Nemesis Main GUI\\data\\meshes");
+	}
+	else
+	{
+		path = bf::path(bf::current_path());
+	}
 
 	if (wordFind(path.string(), "\\Data\\") != NOT_FOUND)
 	{
