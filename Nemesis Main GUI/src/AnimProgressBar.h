cc96c4641bbae229c39b5db78d85d1968e5084de
Shikyo Kira <alexandros_ff9@hotmail.com>
2018-09-01 05:55:22 +0800

    issue #70 & added moving wave pattern to the progress bar

--- Nemesis Main GUI/src/AnimProgressBar.h
+++ Nemesis Main GUI/src/AnimProgressBar.h
@@ -4,14 +4,27 @@
 #include <QProgressBar>
 #include <algorithm>
 
+#define MAX_ANIM 10000
+
 class AnimProgressBar : public QProgressBar
 {
 	Q_OBJECT
-
+		
+private:
+	double cTimer = 0.9;
+	double mRunner = -0.2;
+	double gap = 0.2;
+	double opq = 155;
+	QString hue = "120";
+	const QString sat = "255";
+	QString val = "170";
+	
 public:
+	QString font = "";
 	AnimProgressBar(QWidget *parent = nullptr);
 
 public slots:
+	void ForeverLoop();
 	void newValue(int value);
 };
 
