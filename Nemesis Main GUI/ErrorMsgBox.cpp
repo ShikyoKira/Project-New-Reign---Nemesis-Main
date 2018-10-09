aec44a9227e89a29962ddef277a8eb1ad05b322a
Shikyo Kira <alexandros_ff9@hotmail.com>
2018-10-10 01:01:35 +0800

    Added beeping sound for critical error message

--- Nemesis Main GUI/ErrorMsgBox.cpp
+++ Nemesis Main GUI/ErrorMsgBox.cpp
@@ -1,9 +1,11 @@
 #include "ErrorMsgBox.h"
+#include <QApplication>
 
 CEMsgBox::CEMsgBox(QWidget* parent)
 {
 	setAttribute(Qt::WA_DeleteOnClose, true);
 	setIcon(QMessageBox::Critical);
+	QApplication::beep();
 }
 
 CEMsgBox::~CEMsgBox()
