4acd878396765194cc6400841eb13f051911bfcd
Shikyo Kira <alexandros_ff9@hotmail.com>
2019-04-09 03:12:31 +0800

    Opted cross-platform functions

--- lastupdate.cpp
+++ lastupdate.cpp
@@ -1,6 +1,6 @@
 #include "lastupdate.h"
-#include <Windows.h>
 #include <atomic>
+#include <time.h>
 
 using namespace std;
 
@@ -8,31 +8,26 @@ atomic_flag updateLock = ATOMIC_FLAG_INIT;
 
 bool saveLastUpdate(string filename, unordered_map<string, string>& lastUpdate)
 {
-	HANDLE file;
-	FILETIME lastmodified;
-	SYSTEMTIME sysUTC;
-	file = CreateFile(filename.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-
-	if (file == INVALID_HANDLE_VALUE)
+	try
 	{
-		ErrorMessage(2020);
-		return false;
-	}
+		if (!isFileExist(filename)) ErrorMessage(2022);
 
-	if (!GetFileTime(file, NULL, NULL, &lastmodified))
+		std::time_t lastmodified = boost::filesystem::last_write_time(filename);
+		char time1[26];
+		struct tm buf;
+		localtime_s(&buf, &lastmodified);
+		asctime_s(time1, sizeof time1, &buf);
+		string time = time1;
+		time.pop_back();
+
+		while (updateLock.test_and_set(memory_order_acquire));
+		lastUpdate[filename] = time;
+		updateLock.clear(memory_order_release);
+	}
+	catch (...)
 	{
 		ErrorMessage(2022);
-		return false;
 	}
-
-	FileTimeToSystemTime(&lastmodified, &sysUTC);
-	string time = to_string(sysUTC.wDay) + "/" + to_string(sysUTC.wMonth) + "/" + to_string(sysUTC.wYear) + " " + to_string(sysUTC.wHour) + ":" + to_string(sysUTC.wMinute);
-
-	// boost::lock_guard<boost::mutex> fileLock(addAnimLock);		issue #61
-
-	while (updateLock.test_and_set(memory_order_acquire));
-	lastUpdate[filename] = time;
-	updateLock.clear(memory_order_release);
 
 	return true;
 }
