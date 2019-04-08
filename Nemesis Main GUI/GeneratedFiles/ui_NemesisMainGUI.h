96cc4d2da73beff584ff65b97499f890382211ec
Shikyo Kira <alexandros_ff9@hotmail.com>
2019-04-09 03:13:04 +0800

    Registry check added

--- Nemesis Main GUI/GeneratedFiles/ui_NemesisMainGUI.h
+++ Nemesis Main GUI/GeneratedFiles/ui_NemesisMainGUI.h
@@ -9,6 +9,7 @@
 #ifndef UI_NEMESISMAINGUI_H
 #define UI_NEMESISMAINGUI_H
 
+#include <vector>
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QComboBox>
@@ -25,11 +26,122 @@
 #include "ErrorMsgBox.h"
 #include "SettingsSave.h"
 #include "AnimProgressBar.h"
+#include "src\utilities\wstrconvert.h"
 
 QT_BEGIN_NAMESPACE
-	
+
 class Ui_NemesisMainGUIClass
 {
+private:
+	bool deleteKey(LONG nError, HKEY hKey, LPCWSTR achKey, LPCWSTR& keyName)
+	{
+		if (nError == ERROR_SUCCESS)
+		{
+			HKEY innerKey;
+			nError = RegOpenKeyEx(hKey, achKey, NULL, KEY_ALL_ACCESS, &innerKey);
+
+			if (nError == ERROR_SUCCESS)
+			{
+				TCHAR buffer[16383];
+				DWORD size = sizeof(buffer), type = REG_SZ;
+				int counter = 0;
+				std::wstring filename;
+				nError = RegQueryValueEx(innerKey, TEXT("directory"), NULL, &type, LPBYTE(buffer), &size);
+
+				if (nError == ERROR_SUCCESS)
+				{
+					filename = buffer;
+
+					if (!boost::filesystem::exists(filename + L"\\mod"))
+					{
+						return true;
+					}
+					else
+					{
+						LPWSTR curpath = new WCHAR[WCHAR_MAX];
+						GetModuleFileNameW(NULL, curpath, WCHAR_MAX);
+						std::wstring wstrdata = boost::filesystem::path(curpath).parent_path().wstring();
+						curpath = const_cast<LPWSTR>(wstrdata.c_str());
+
+						if (filename == curpath)
+						{
+							keyName = achKey;
+							subkeys = std::to_wstring(CRC32Convert(wstrConv.to_bytes(curpath).c_str()) % 99999999);
+						}
+					}
+				}
+				else return true;
+			}
+			else return true;
+		}
+		else return true;
+
+		return false;
+	}
+
+	void checkKey(HKEY hKey, LPCWSTR& keyName)
+	{
+		LONG nError;
+		DWORD    cbName;                   // size of name string 
+		TCHAR    achClass[MAX_PATH] = TEXT("");  // buffer for class name 
+		DWORD    cchClassName = MAX_PATH;  // size of class string 
+		DWORD    cSubKeys = 0;               // number of subkeys 
+		DWORD    cbMaxSubKey;              // longest subkey size 
+		DWORD    cchMaxClass;              // longest class string 
+		DWORD    cValues;              // number of values for key 
+		DWORD    cchMaxValue;          // longest value name 
+		DWORD    cbMaxValueData;       // longest value data 
+		DWORD    cbSecurityDescriptor; // size of security descriptor 
+		FILETIME ftLastWriteTime;      // last write time 
+
+		DWORD i;
+		std::vector<std::wstring> deletingValues;
+
+		// Get the class name and the value count. 
+		nError = RegQueryInfoKey(
+			hKey,                    // key handle 
+			achClass,                // buffer for class name 
+			&cchClassName,           // size of class string 
+			NULL,                    // reserved 
+			&cSubKeys,               // number of subkeys 
+			&cbMaxSubKey,            // longest subkey size 
+			&cchMaxClass,            // longest class string 
+			&cValues,                // number of values for this key 
+			&cchMaxValue,            // longest value name 
+			&cbMaxValueData,         // longest value data 
+			&cbSecurityDescriptor,   // security descriptor 
+			&ftLastWriteTime);       // last write time 
+
+									 // Enumerate the subkeys, until RegEnumKeyEx fails.
+
+									 // Enumerate the key values. 
+		if (cSubKeys)
+		{
+			for (i = 0; i < cSubKeys; i++)
+			{
+				TCHAR    achKey[255];   // buffer for subkey name
+				cbName = 255;
+				nError = RegEnumKeyEx(hKey, i, achKey, &cbName, NULL, NULL, NULL, &ftLastWriteTime);
+
+				if (deleteKey(nError, hKey, achKey, keyName)) deletingValues.push_back(achKey);
+			}
+		}
+
+		for (auto& each : deletingValues)
+		{
+			nError = RegDeleteKeyEx(hKey, each.c_str(), KEY_ALL_ACCESS, 0);
+
+			if (nError != ERROR_SUCCESS) DebugLogging("Failed to delete registry key");
+		}
+
+		if (keyName == NULL)
+		{
+			LPSTR curpath = new CHAR[CHAR_MAX];
+			GetModuleFileNameA(NULL, curpath, CHAR_MAX);
+			subkeys = std::to_wstring(CRC32Convert(curpath) % 99999999);
+		}
+	}
+
 public:
 	QGridLayout *gridLayout;
 	BehaviorListView *modView;
@@ -45,6 +157,7 @@ public:
 	QPushButton *buttonUpdate;
 	DebugMsg* DMsg;
 	DummyLog* DLog;
+	std::wstring subkeys;
 
 	void setupUi(QWidget *NemesisMainGUIClass)
 	{
@@ -101,8 +214,6 @@ public:
 
 		animProgressBar = new AnimProgressBar(NemesisMainGUIClass);
 		animProgressBar->setObjectName(QStringLiteral("animProgressBar"));
-		animProgressBar->setMaximum(MAX_ANIM);
-		animProgressBar->setValue(0);
 		animProgressBar->setAlignment(Qt::AlignCenter);
 		animProgressBar->setTextVisible(false);
 
@@ -155,6 +266,97 @@ public:
 		buttonLaunch->setFont(font1);
 		buttonLaunch->setCursor(QCursor(Qt::PointingHandCursor));
 
+		{
+			HKEY hKey;
+			DWORD dwDisposition;
+			LONG nError = RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SOFTWARE\\Nemesis"), NULL, KEY_ALL_ACCESS, &hKey);
+
+			if (nError == ERROR_SUCCESS)
+			{
+				LPCWSTR keyName = NULL;
+				checkKey(hKey, keyName);
+				LPCWSTR s_subkey = subkeys.c_str();
+
+				if (keyName != NULL)
+				{
+					HKEY innerKey;
+					nError = RegOpenKeyEx(hKey, s_subkey, NULL, KEY_ALL_ACCESS, &innerKey);
+
+					if (nError == ERROR_SUCCESS)
+					{
+						DWORD data, size = sizeof(data), type = REG_DWORD;
+						nError = RegQueryValueEx(innerKey, TEXT("first"), NULL, &type, (LPBYTE)&data, &size);
+
+						if (nError == ERROR_SUCCESS)
+						{
+							if (data == 1)
+							{
+								buttonLaunch->setDisabled(true);
+							}
+						}
+						else  DebugLogging("Failed to update registry key value");
+					}
+					else  DebugLogging("Failed to open registry key");
+
+					RegCloseKey(innerKey);
+				}
+				else
+				{
+					buttonLaunch->setDisabled(true);
+					HKEY innerKey;
+					DWORD data = 1;
+					nError = RegSetKeyValue(hKey, s_subkey, TEXT("first"), REG_DWORD, &data, sizeof(data));
+
+					if (nError != ERROR_SUCCESS) DebugLogging("Failed to create registry key value");
+
+					nError = RegOpenKeyEx(hKey, s_subkey, NULL, KEY_ALL_ACCESS, &innerKey);
+
+					if (nError == ERROR_SUCCESS)
+					{
+						LPWSTR strdata = new WCHAR[WCHAR_MAX];
+						GetModuleFileNameW(NULL, strdata, WCHAR_MAX);
+						std::wstring wstrdata = boost::filesystem::path(strdata).parent_path().wstring();
+						strdata = const_cast<LPWSTR>(wstrdata.c_str());
+						nError = RegSetValueEx(innerKey, TEXT("directory"), 0, REG_SZ, LPCBYTE(strdata), wcslen(strdata) * sizeof(TCHAR));
+
+						if (nError != ERROR_SUCCESS) DebugLogging("Failed to create registry key value");
+					}
+					else  DebugLogging("Failed to open registry key");
+
+					RegCloseKey(innerKey);
+				}
+			}
+			else
+			{
+				buttonLaunch->setDisabled(true);
+				LPSTR curpath = new CHAR[CHAR_MAX];
+				GetModuleFileNameA(NULL, curpath, CHAR_MAX);
+				std::wstring tempkey = std::to_wstring(CRC32Convert(curpath) % 99999999);
+				nError = RegCreateKeyEx(HKEY_LOCAL_MACHINE, (L"SOFTWARE\\Nemesis\\" + tempkey).c_str(), NULL, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL,
+					&hKey, NULL);
+
+				if (nError == ERROR_SUCCESS)
+				{
+					DWORD data = 1, size = sizeof(data), type = REG_DWORD;
+					nError = RegSetKeyValue(hKey, L"", TEXT("first"), type, &data, size);
+
+					if (nError != ERROR_SUCCESS) DebugLogging("Failed to create registry key value");
+
+					LPWSTR strdata = new WCHAR[WCHAR_MAX];
+					GetModuleFileNameW(NULL, strdata, WCHAR_MAX);
+					std::wstring wstrdata = boost::filesystem::path(strdata).parent_path().wstring();
+					strdata = const_cast<LPWSTR>(wstrdata.c_str());
+					nError = RegSetValueEx(hKey, TEXT("directory"), 0, REG_SZ, LPCBYTE(strdata), wcslen(strdata) * sizeof(TCHAR));
+
+					if (nError == ERROR_SUCCESS) subkeys = tempkey;
+					else DebugLogging("Failed to create registry key value");
+				}
+				else DebugLogging("Failed to create registry key");
+			}
+
+			RegCloseKey(hKey);
+		}
+
 		gridLayout->addWidget(buttonLaunch, 4, 1, 1, 7);
 
 		comboBox = new QComboBox(NemesisMainGUIClass);
@@ -189,7 +391,6 @@ public:
 
 		gridLayout->addWidget(buttonUpdate, 5, 4, 1, 5);
 
-
 		retranslateUi(NemesisMainGUIClass);
 
 		QMetaObject::connectSlotsByName(NemesisMainGUIClass);
@@ -197,11 +398,23 @@ public:
 
 	void retranslateUi(QWidget *NemesisMainGUIClass)
 	{
-		NemesisMainGUIClass->setWindowTitle(QApplication::translate("NemesisMainGUIClass", "Nemesis Ultimate Behavior Engine", nullptr));
-
 		backUp(NemesisMainGUIClass);
 		GetSettings();
 		reset(NemesisMainGUIClass);
+
+		try
+		{
+			skyrimDataPath = new DataPath;
+		}
+		catch (nemesis::exception)
+		{
+			buttonCheck->setDisabled(true);
+			buttonLaunch->setDisabled(true);
+			buttonUpdate->setDisabled(true);
+			modView->setDisabled(true);
+			comboBox->setDisabled(true);
+			error = false;
+		}
 
 		QObject::connect(buttonLaunch, SIGNAL(released()), NemesisMainGUIClass, SLOT(handleButton1()));
 		QObject::connect(buttonUpdate, SIGNAL(released()), NemesisMainGUIClass, SLOT(handleButton2()));
@@ -217,6 +430,9 @@ public:
 				indexlist->push_back(modView->model()->index(i, j));
 			}
 		}
+
+		animProgressBar->setMaximum(MAX_ANIM);
+		animProgressBar->setValue(0);
 
 		modView->setColumnWidth(0, 380);
 		modView->setColumnWidth(1, 75);
