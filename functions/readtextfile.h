ed9369ca2b7a4876deefc5de0d073a0055dd889c
Shikyo Kira <alexandros_ff9@hotmail.com>
2019-04-23 04:16:27 +0800

    Fixed issue #93 by changing from C based file reader to QT based

--- functions/readtextfile.h
+++ functions/readtextfile.h
@@ -1,26 +1,51 @@
 #ifndef READTEXTFILE_H_
 #define READTEXTFILE_H_
 
+#include <QtCore\QFile.h>
+
 struct FileReader
 {
-	FILE* file;
+	QFile file;
 
 	FileReader(std::string filename)
 	{
-		fopen_s(&file, filename.c_str(), "r");
+		file.setFileName(QString::fromStdString(filename));
 	}
 
 	~FileReader()
 	{
-		if (file)
+		if (GetFile())
 		{
-			fclose(file);
+			file.close();
 		}
 	}
 
-	FILE* GetFile()
+	bool GetFile()
 	{
-		return file;
+		return file.open(QIODevice::ReadOnly);
+	}
+
+	bool GetLines(std::string& line)
+	{
+		while (!file.atEnd())
+		{
+			QString qline = file.readLine();
+			line = qline.toStdString();
+
+			if (line.length() > 0)
+			{
+				if (line.back() == '\n' || line.back() == '\r') line.pop_back();
+
+				if (line.length() > 0)
+				{
+					if (line.back() == '\r' || line.back() == '\n') line.pop_back();
+				}
+			}
+
+			return true;
+		}
+
+		return false;
 	}
 };
 
