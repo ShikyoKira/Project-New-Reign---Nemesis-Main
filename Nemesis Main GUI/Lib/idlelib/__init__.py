b4214116d0ba41623d0ff78685bf22debb48eb85
Shikyo Kira <alexandros_ff9@hotmail.com>
2019-07-16 04:54:36 +0800

    Added python library, removed lua library

---
+++ Nemesis Main GUI/Lib/idlelib/__init__.py
@@ -0,0 +1,9 @@
+"""The idlelib package implements the Idle application.
+
+Idle includes an interactive shell and editor.
+Use the files named idle.* to start Idle.
+
+The other files are private implementations.  Their details are subject to
+change.  See PEP 434 for more.  Import them at your own risk.
+"""
+testing = False  # Set True by test.test_idle.
