ba27b682e0afc9373007d4f98dba631e000cd7fc
Shikyo Kira <alexandros_ff9@hotmail.com>
2019-12-29 05:42:51 +0800

    Removed win api force kill app

--- functions/renew.cpp
+++ functions/renew.cpp
@@ -1,9 +1,28 @@
 #include "renew.h"
-#include "Nemesis Main GUI\src\utilities\filerelease.h"
 
 #pragma warning(disable:4503)
 
 using namespace std;
+
+void tryDelete(string file, bool xml, int repeated = 0)
+{
+	if (repeated > 100)
+	{
+		if (xml)
+		{
+			WarningMessage(1009, file);
+		}
+		else
+		{
+			WarningMessage(1006);
+		}
+	}
+
+	if (!boost::filesystem::remove(file))
+	{
+		tryDelete(file, xml, repeated + 1);
+	}
+}
 
 bool DeleteFileFolder(string directory, string file, bool xml)
 {
@@ -20,18 +39,7 @@ bool DeleteFileFolder(string directory, string file, bool xml)
 		}
 	}
 
-	if (ReleaseLockedFile(directory + file) && !boost::filesystem::remove(directory + file))
-	{
-		if (xml)
-		{
-			WarningMessage(1009, file);
-		}
-		else
-		{
-			WarningMessage(1006);
-		}
-	}
-
+	tryDelete(directory + file, xml);
 	return true;
 }
 
