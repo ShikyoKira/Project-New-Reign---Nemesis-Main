d84790d88c993f9ab837da5b7e91c8529d07589b
Shikyo Kira <alexandros_ff9@hotmail.com>
2020-03-16 05:05:51 +0800

    String split, custom split symbol support

--- add animation/stringsplit.h
+++ add animation/stringsplit.h
@@ -1,8 +1,9 @@
 #ifndef STRINGSPLIT_H_
 #define STRINGSPLIT_H_
 
-#include "Global.h"
+typedef std::vector<std::string> vecstr;
 
 void StringSplit(std::string line, vecstr& container);
+void StringSplit(std::string line, vecstr& container, std::string anyChar);
 
 #endif
