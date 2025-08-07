05f0ea3c34cc10cfa8f575586308f9949d21a2f6
Shikyo Kira <alexandros_ff9@hotmail.com>
2025-08-08 06:44:39 +0800

    Fixed @AnimationFilePath missing out parent directory

--- Nemesis_Core_Engine/src/Core/Template/TemplateClass.cpp
+++ Nemesis_Core_Engine/src/Core/Template/TemplateClass.cpp
@@ -624,7 +624,8 @@ nemesis::TemplateClass::CreateRequest(const std::string& list_name,
 
     if (!(ss >> component)) return nullptr;
 
-    request->SetAnimationFilePath(component);
+    // window's path separator is used for consistency
+    request->SetAnimationFilePath(nemesis::to_utf8_string(filepath.parent_path().stem()) + "\\" + component);
 
     while (ss >> component)
     {
